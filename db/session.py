309b05d64627b51885ead2308748c4610090f92b
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 12:00:37 +0530

    Updated session file

--- db/session.py
+++ db/session.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+dotenv_path = '.env'
+load_dotenv(dotenv_path)
 
 engine = create_engine(
     os.environ["DATABASE_URL"],
