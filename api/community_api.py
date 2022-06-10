1ba992a80fcbf34b7b81020b49c875b2e91af629
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 12:41:44 +0530

    added api for listing community members

--- api/community_api.py
+++ api/community_api.py
@@ -5,7 +5,7 @@ from schemas.community_schema import CheckUserInCommunityPayload, ListCommunityM
 from models import User
 
 from utilities.authentication.authentication_helper import jwt_auth_required
-from utilities.common.common_helper import get_community_members_map
+from utilities.common.common_helper import get_community_members_map, get_community_members
 
 from utilities.database import database_helper
 
@@ -18,11 +18,10 @@ async def check_user_in_community(user: User = Depends(jwt_auth_required),
     return CheckUserInCommunityPayload(status=status.HTTP_200_OK, is_in_community=is_in_community)
 
 
-@router.post('add-user-in-community', summary="Adds user to community", response_model=ListCommunityMembersPayload)
+@router.post('/add-user-in-community', summary="Adds user to community", response_model=ListCommunityMembersPayload)
 async def add_user_in_community(user: User = Depends(jwt_auth_required), 
                         db = Depends(database_helper.get_db)) -> ListCommunityMembersPayload:
-    members = database_helper.get_community_members(db)
-    members = [CommunityMember(email=member.email) for member in members ]
+    members = get_community_members(db)
     community_members_map = get_community_members_map(members=members)
     
     if user.email in community_members_map:
@@ -33,3 +32,9 @@ async def add_user_in_community(user: User = Depends(jwt_auth_required),
     members.append(CommunityMember(email=user.email))
     return ListCommunityMembersPayload(status=200, members=members)
 
+
+@router.get('/show-community-members', summary="Lists community members", response_model=ListCommunityMembersPayload)
+async def add_user_in_community(user: User = Depends(jwt_auth_required), 
+                        db = Depends(database_helper.get_db)) -> ListCommunityMembersPayload:
+    members = get_community_members(db)
+    return ListCommunityMembersPayload(status=200, members=members)
