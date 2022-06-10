9fcc6d143da9ccde908ebeaed1ae670601d2d652
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 12:41:17 +0530

    Added common function to get members

--- utilities/common/common_helper.py
+++ utilities/common/common_helper.py
@@ -1,6 +1,9 @@
 from typing import Dict, List
+from sqlalchemy.orm.session import Session
 
 from models import CommunityMember
+from schemas import community_schema
+from utilities.database import database_helper
 
 def get_community_members_map(members: List[CommunityMember]) -> Dict[str, bool]:
     map: Dict[str, bool] = dict()
@@ -8,3 +11,8 @@ def get_community_members_map(members: List[CommunityMember]) -> Dict[str, bool]
         map[member.email] = True
     return map
 
+
+def get_community_members(db: Session) -> List[CommunityMember]:
+    members = database_helper.get_community_members(db)
+    members = [community_schema.CommunityMember(email=member.email) for member in members ]
+    return members
