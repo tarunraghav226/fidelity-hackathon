0b94b54668e20e0483c1f620a970c9f64a850ff4
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 12:29:53 +0530

    Added new schemas

--- schemas/community_schema.py
+++ schemas/community_schema.py
@@ -1,5 +1,15 @@
-from pydantic import BaseModel
+from typing import List
+from pydantic import BaseModel, EmailStr
 
 class CheckUserInCommunityPayload(BaseModel):
     status: int
     is_in_community: bool
+
+
+class CommunityMember(BaseModel):
+    email: EmailStr
+
+
+class ListCommunityMembersPayload(BaseModel):
+    status: int
+    members: List[CommunityMember]
