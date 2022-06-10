b5689b20a65d73096c0e4391a4fcf7e71e89271b
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 10:56:22 +0530

    Updated user model

--- models.py
+++ models.py
@@ -6,6 +6,6 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"    
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     email = Column(String, unique=True)
     password = Column(String)
