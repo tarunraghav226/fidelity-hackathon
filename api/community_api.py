5e06a8a667754fada397783e551255d9d12d5698
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 12:30:25 +0530

    Added api to add member in community

--- api/community_api.py
+++ api/community_api.py
@@ -1,22 +1,35 @@
-import imp
-from fastapi import APIRouter, Depends, FastAPI, status, HTTPException
-from fastapi.responses import RedirectResponse
+from fastapi import APIRouter, Depends, status
 
-from schemas.authentication_schema import UserSignUpPayload
-from schemas.user_schema import UserCreate, UserCreateResponse
-from schemas.community_schema import CheckUserInCommunityPayload
+from schemas.community_schema import CheckUserInCommunityPayload, ListCommunityMembersPayload, CommunityMember
 
 from models import User
 
 from utilities.authentication.authentication_helper import jwt_auth_required
-from uuid import uuid4
+from utilities.common.common_helper import get_community_members_map
 
 from utilities.database import database_helper
 
 router = APIRouter(prefix="/community", tags=["communities"])
 
-@router.post('/check-user-in-community', summary="Checks user has joined any community or not", response_model=CheckUserInCommunityPayload)
+@router.get('/check-user-in-community', summary="Checks user has joined any community or not", response_model=CheckUserInCommunityPayload)
 async def check_user_in_community(user: User = Depends(jwt_auth_required), 
                         db = Depends(database_helper.get_db)) -> CheckUserInCommunityPayload:
     is_in_community = database_helper.is_user_in_community(db, user)
     return CheckUserInCommunityPayload(status=status.HTTP_200_OK, is_in_community=is_in_community)
+
+
+@router.post('add-user-in-community', summary="Adds user to community", response_model=ListCommunityMembersPayload)
+async def add_user_in_community(user: User = Depends(jwt_auth_required), 
+                        db = Depends(database_helper.get_db)) -> ListCommunityMembersPayload:
+    members = database_helper.get_community_members(db)
+    members = [CommunityMember(email=member.email) for member in members ]
+    community_members_map = get_community_members_map(members=members)
+    
+    if user.email in community_members_map:
+        print(members)
+        return ListCommunityMembersPayload(status=200, members=members)
+    
+    database_helper.add_user_in_community(db, user.email)
+    members.append(CommunityMember(email=user.email))
+    return ListCommunityMembersPayload(status=200, members=members)
+
