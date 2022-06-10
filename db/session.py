617e72d388991e20194125801f873b09a0a8930b
tarunraghav226 <tarun.raghav226@gmail.com>
2022-06-10 13:51:17 +0530

    Bug fix

--- db/session.py
+++ db/session.py
@@ -8,7 +8,7 @@ dotenv_path = '.env'
 load_dotenv(dotenv_path)
 
 engine = create_engine(
-    os.environ["DATABASE_URL"],
+    "postgresql://jcnhbrnyselsea:f6b18d6e67c448fbccf89de1e550e439d1d1850cffc98bb7112f7bf28dbc58d6@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/ddi9g9es2dg3r0",
     pool_pre_ping=True
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
